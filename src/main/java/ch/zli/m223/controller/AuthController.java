a6030a4b022e939541ec63524cfc7bae70db9568
noahburren <noah.burren@bluewin.ch>
2025-11-24 12:41:25 +0000

    cors

--- src/main/java/ch/zli/m223/controller/AuthController.java
+++ src/main/java/ch/zli/m223/controller/AuthController.java
@@ -3,11 +3,9 @@ package ch.zli.m223.controller;
 import ch.zli.m223.service.AuthService;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.*;
-import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
 
 @Path("/auth")
-@Consumes(MediaType.APPLICATION_JSON)
-@Produces(MediaType.APPLICATION_JSON)
 public class AuthController {
 
     @Inject
@@ -15,10 +13,15 @@ public class AuthController {
 
     @POST
     @Path("/login")
-    public String login(LoginRequest req) {
-        return authService.login(req.email, req.password)
-                .getEntity()
-                .toString();
+    public Response login(LoginRequest req) {
+        return authService.login(req.email, req.password);
+    }
+
+    // CORS Preflight Handler
+    @OPTIONS
+    @Path("/login")
+    public Response preflight() {
+        return Response.ok().build();
     }
 
     public static class LoginRequest {
--- src/main/java/ch/zli/m223/service/AuthService.java
+++ src/main/java/ch/zli/m223/service/AuthService.java
@@ -1,11 +1,11 @@
 package ch.zli.m223.service;
 
+import ch.zli.m223.model.UserModel;
 import io.smallrye.jwt.build.Jwt;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.core.Response;
-
-import ch.zli.m223.model.UserModel;
+import org.mindrot.jbcrypt.BCrypt;
 
 @ApplicationScoped
 public class AuthService {
@@ -14,20 +14,22 @@ public class AuthService {
     UserService userService;
 
     public Response login(String email, String password) {
+
         UserModel user = userService.findByEmail(email);
 
         if (user == null) {
             return Response.status(401).entity("User not found").build();
         }
 
-        boolean pwValid = org.mindrot.jbcrypt.BCrypt.checkpw(password, user.password);
-        if (!pwValid) {
+        boolean validPw = BCrypt.checkpw(password, user.password);
+
+        if (!validPw) {
             return Response.status(401).entity("Invalid password").build();
         }
 
         String token = Jwt.upn(user.email)
                 .issuer("https://example.com/issuer")
-                .groups(user.role) // Admin oder User
+                .groups(user.role)
                 .sign();
 
         return Response.ok(token).build();
